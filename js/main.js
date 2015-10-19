fa360f554ba4386f006e24e83df03f87d495f3dc
Erica Chase <ericachase11@gmail.com>
2015-10-18 19:58:32 -0500

    show/hide text on keypress

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-function blinker()
-{
+function blinker() {
 	for (i=0;i<100;i++) {
 	    if(document.getElementsByClassName('blink')[i]) {
 	        var d = document.getElementsByClassName('blink')[i] ;
@@ -8,3 +7,12 @@ function blinker()
 	    }
 	};
 }
+
+var toShow = true;
+$(document).keydown(function(keyEvent) {
+    if(keyEvent.keyCode == 13){
+            $('#1').toggle(!toShow);
+            $('#div2').toggle(!toShow);
+        toShow = !toShow;
+    }
+});
